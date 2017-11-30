b6499fe8c7a54418429e3573d95ee1033d64741f
wumingyuan <785006487@qq.com>
2017-11-30 14:48:40 +0800

    woman

---
+++ WMYVIew/woman.h
@@ -0,0 +1,13 @@
+//
+//  woman.h
+//  WMYView
+//
+//  Created by apple on 2017/11/30.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface woman : NSObject
+
+@end
---
+++ WMYVIew/woman.m
@@ -0,0 +1,13 @@
+//
+//  woman.m
+//  WMYView
+//
+//  Created by apple on 2017/11/30.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import "woman.h"
+
+@implementation woman
+
+@end
