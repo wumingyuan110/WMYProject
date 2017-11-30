82819e5e39359b1726deb4b7b8b76e55db47d3eb
wumingyuan <785006487@qq.com>
2017-11-30 14:00:06 +0800

    Person

---
+++ WMYVIew/WMYVIew/WMPerson.h
@@ -0,0 +1,13 @@
+//
+//  WMPerson.h
+//  WMYVIew
+//
+//  Created by apple on 2017/11/30.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WMPerson : NSObject
+
+@end
---
+++ WMYVIew/WMYVIew/WMPerson.m
@@ -0,0 +1,13 @@
+//
+//  WMPerson.m
+//  WMYVIew
+//
+//  Created by apple on 2017/11/30.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import "WMPerson.h"
+
+@implementation WMPerson
+
+@end
