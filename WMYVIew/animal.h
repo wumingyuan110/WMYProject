2a638fa4f637f7e4f237bce78c9b2a662ae478ce
wumingyuan <785006487@qq.com>
2017-11-30 14:47:45 +0800

    animal

---
+++ WMYVIew/animal.h
@@ -0,0 +1,13 @@
+//
+//  animal.h
+//  WMYView
+//
+//  Created by apple on 2017/11/30.
+//  Copyright © 2017年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface animal : NSObject
+
+@end
