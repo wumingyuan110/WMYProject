0001004b161c58981ee5a669a037f0ac3d4c04a8
wumingyuan <785006487@qq.com>
2017-11-30 13:53:35 +0800

    backrroundcolor

--- WMYVIew/WMYVIew/ViewController.m
+++ WMYVIew/WMYVIew/ViewController.m
@@ -16,6 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor colorWithWhite:0.5 alpha:1];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
